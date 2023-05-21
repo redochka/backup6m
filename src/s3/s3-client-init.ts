5f69854777297e6a49321363ade2caaf274596a1
reda <reda.abdi@gmail.com>
2023-05-21 15:54:43 +0200

    invalidate exposed credentials

--- src/s3/s3-client-init.ts
+++ src/s3/s3-client-init.ts
@@ -15,8 +15,6 @@ const s3Client = new S3({
     credentials: {
         accessKeyId: accessKeyId,
         secretAccessKey: secretAccessKey
-        // accessKeyId: "DO008HWBAPDU4YN8MZ6G",
-        // secretAccessKey: "FvACP8ZYnSpEQCipAG3/vBrPkbg6h5OlA2CsSor9kHA"
     }
     // not working, bug issue created on aws
     /*    credentials: fromIni({
