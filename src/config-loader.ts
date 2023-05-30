85859c39092ca579bfd9eaf1ebd6b1be31a87a37
reda <reda.abdi@gmail.com>
2023-05-30 18:30:18 +0200

    Use import instead of require

--- src/config-loader.ts
+++ src/config-loader.ts
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+import fs from "fs";
+import path from "path";
 
 export function loadConfigurationFiles() {
     const myPath = "./mount/b6m-config";
